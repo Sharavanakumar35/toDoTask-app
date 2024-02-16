b7f0dc8f352465f02e378985e8df6d8b77b22b59
Sharavanakumar35 <shark.35.kumar@gmail.com>
2024-02-16 10:21:04 +0530

    ToDo card changes

--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -5,17 +5,17 @@ const ToDoCard = ({name, description, status, id, openEdit, deleteToDoList}) => 
         <div className="card todo-card bg-todo m-2">
             <div className="card-body">
                 <div className='d-flex justify-content-between mb-2'>
-                    <h5 className="card-title text-left">Name:</h5>
-                    <h5 className="card-title text-right"> {name}</h5>
+                    <h6 className="card-title text-left">Name:</h6>
+                    <h6 className="card-title text-right"> {name}</h6>
                 </div>
 
                 <div className='d-flex justify-content-between mb-2'>
-                    <p className="card-text text-left me-4">Description:</p>
+                    <h6 className="card-text text-left me-4">Description:</h6>
                     <p className="card-text description-limit text-right">{description}</p>
                 </div>
 
                 <div className='d-flex justify-content-between align-items-baseline mb-2'>
-                    <p className="card-text text-left">Status:</p>
+                    <h6 className="card-text text-left">Status:</h6>
                     <form className='text-right'>
                         <select name="status" id="status" value={status} className='form-select text-white' style={{backgroundColor: status === 'Completed' ? '#13ad89' : '#dc3545'}}>
                             <option value={status}>{status === 'InComplete' ? 'Not Completed' : status}</option>
